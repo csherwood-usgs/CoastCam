52fb111d246d929f1ae150356fad637dc7679474
Chris Sherwood on Workspaces <csherwood@usgs.gov>
2021-04-01 21:50:21 +0000

    changed aws script

--- awss/latest.sh
+++ awss/latest.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo "caco-01"
-echo "latest:"
-aws s3 ls s3://cmgp-coastcam/cameras/caco-01/latest --recursive --profile "coastcam" | sort | tail -n 10
-echo "products:"
-aws s3 ls s3://cmgp-coastcam/cameras/caco-01/products --recursive --profile "coastcam" | sort | tail -n 12
-echo ""
 echo "caco-02"
 echo "latest:"
 aws s3 ls s3://cmgp-coastcam/cameras/caco-02/latest --recursive --profile "coastcam" | sort | tail -n 10
 echo "products:"
 aws s3 ls s3://cmgp-coastcam/cameras/caco-02/products --recursive --profile "coastcam" | sort | tail -n 10
+echo ""
+echo "caco-01"
+echo "latest:"
+aws s3 ls s3://cmgp-coastcam/cameras/caco-01/latest --recursive --profile "coastcam" | sort | tail -n 10
+echo "products (may take a while):"
+aws s3 ls s3://cmgp-coastcam/cameras/caco-01/products --recursive --profile "coastcam" | sort | tail -n 12
