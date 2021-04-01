dbb478c9d62c7ff1a4a1c9a50c1896d189076e51
csherwood-usgs <csherwood@usgs.gov>
2021-04-01 18:44:48 -0400

    shell script

--- awss/latest.sh
+++ awss/latest.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+echo "...working"
 echo "caco-02"
-echo "latest:"
-aws s3 ls s3://cmgp-coastcam/cameras/caco-02/latest --recursive --profile "coastcam" | sort | tail -n 10
-echo "products:"
-aws s3 ls s3://cmgp-coastcam/cameras/caco-02/products --recursive --profile "coastcam" | sort | tail -n 10
+echo "latest"
+aws s3 ls s3://cmgp-coastcam/cameras/caco-02/latest --recursive --profile "coastcam" | sort | tail -n 10 > latest.txt
+echo "products"
+aws s3 ls s3://cmgp-coastcam/cameras/caco-02/products --recursive --profile "coastcam" | sort | tail -n 10 >> latest.txt
 echo ""
 echo "caco-01"
-echo "latest:"
-aws s3 ls s3://cmgp-coastcam/cameras/caco-01/latest --recursive --profile "coastcam" | sort | tail -n 10
+echo "latest"
+aws s3 ls s3://cmgp-coastcam/cameras/caco-01/latest --recursive --profile "coastcam" | sort | tail -n 10 >> latest.txt
 echo "products (may take a while):"
-aws s3 ls s3://cmgp-coastcam/cameras/caco-01/products --recursive --profile "coastcam" | sort | tail -n 12
+aws s3 ls s3://cmgp-coastcam/cameras/caco-01/products --recursive --profile "coastcam" | sort | tail -n 12 >> latest.txt
+
+cat latest.txt
+exit 0
