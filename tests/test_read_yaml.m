573f46fb88ab4c748f362c975f92a688256a862f
csherwood-usgs <csherwood@usgs.gov>
2021-11-10 16:44:11 -0500

    added Matlab yml reader

---
+++ tests/test_read_yaml.m
@@ -0,0 +1,4 @@
+% test reading .yml file with intrinsics data
+% using this function: https://github.com/llerussell/ReadYAML/blob/master/ReadYaml.m
+yfn = '../data/2021-02-25_CACO02_C1_IO.yml'
+s = ReadYaml(yfn)
